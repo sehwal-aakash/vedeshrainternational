89246f7d9005cd80138bade3cd265fa4092b3606
Aakash Sharma <sehwal.aakash@gmail.com>
2025-08-15 22:19:02 +0530

    Changes

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    protected $table = 'blogs';
+    protected $table = 'blog';
 
     protected $fillable = [
         'title',
