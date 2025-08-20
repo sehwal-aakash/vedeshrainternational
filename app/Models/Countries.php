b5a6743c22714ba0e84123bb4c29f2ca1bc48426
Aakash Sharma <sehwal.aakash@gmail.com>
2025-08-20 23:59:23 +0530

    Change name for Countries Model

--- app/Models/Countries.php
+++ app/Models/Countries.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Country extends Model
+class Countries extends Model
 {
     use HasFactory;
 
